0ee816e240a0fe25e1be910a4dd51666506201d1
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2025-12-15 10:47:38 +0200

    fixed bug

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -7,9 +7,14 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls")),
-    path("login/", auth_views.LoginView.as_view(
-        template_name="registration/login.html"
-    ), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"),
-         name="logout"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page="login"),
+        name="logout"
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
